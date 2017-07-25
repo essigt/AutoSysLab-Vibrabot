af06f34c0f3673fcdc7ca2235919cf6fa8632957
essigt <tim.essig@freight-crush.de>
2017-07-25 11:35:27 +0200

    Use new Vibrabot MasterSlave module

--- SwarmBot.cpp
+++ SwarmBot.cpp
@@ -5,18 +5,19 @@
 
 void SwarmBot::init(void) {
   Vibrabot::init();
-  
+
   this->leftMotor.setPwmValue(120);
   this->rightMotor.setPwmValue(120);
-  
+
   this->rgbLed.setLEDColor(&ColorOrange);
+
+  this->masterSlave.init(this->communicator);
 }
+
 
 void SwarmBot::process(void)
 {
-  //  adc.dump();
-
-
+  ComToken * token;
 
   if (this->mainClock.isTick())
   {
@@ -24,92 +25,27 @@ void SwarmBot::process(void)
     this->rgbLed.process();
     this->processAnalogData();
 
-
-    //Master/Slave Proof of Concept
-    this->sendMsg();
-    this->checkMaster();
-
+    this->masterSlave.process();
 
     
-    this->checkLight();
-
-
-    
-    if (counter == 1000) {
-      counter = 0;
-    }
-    counter++;
-
-  }
-
-
-}
-
-
-extern void printData(char * text, uint16_t data);
-
-
-void SwarmBot::checkMaster(void) {
-
-  ComToken * token2;
-
-  if (this->communicator.hasTokenToRead())
-  {
-    token2 =  this->communicator.readNextToken();
-
-    if (token2->getChannel() == 0) {
-      Serial.print("Master Token received");
-      Serial.print(token2->getHeader());
-      Serial.print(" : ");
-      Serial.print(token2->getData());
-
-      Serial.print(" C : ");
-      Serial.print(token2->getChannel());
-
-      Serial.print(" S: ");
-      Serial.println(token2->getStrength());
-
-
-      noMasterTokenCounter = 1500;
-      state = STATE_SLAVE;
+    if (this->communicator.hasTokenToRead())
+    {
+      token =  this->communicator.readNextToken();
+      
+      //TODO Only pass token if it is a MasterToken
+      if(token->getHeader() == MS_TOKEN_HEADER) {
+        this->masterSlave.passToken(token);
+      }
     }
 
+    this->updateLED();
 
-  } else {
-
-    noMasterTokenCounter--;
-    if (noMasterTokenCounter == 0) {
-      state = STATE_MASTER;
-    }
-  }
-
-
-}
-
-void SwarmBot::sendMsg(void) {
-  if (counter % 500 == 0) { //twice a second
-
-
-    //Send Message to Slave
-    ComToken  token;
-    token.init(COM_TOKEN_RESPONSE);
-    token.setHeader(0x01);
-    token.setData(0x02);
-
-
-    /*Serial.print("Token send :");
-      Serial.print(token.getHeader());
-      Serial.print(" : ");
-      Serial.println(token.getData());*/
-
-    this->communicator.send(0x08, token);
-    Serial.println("Master token send");
 
   }
 }
 
 
-void SwarmBot::checkLight(void)
+void SwarmBot::updateLED(void)
 {
   uint16_t lightLeft;
   uint16_t lightRight;
@@ -118,50 +54,19 @@ void SwarmBot::checkLight(void)
   lightRight = this->adc.getBrightness(1);
 
 
-  if (state == STATE_SLAVE) {
+  if (this->masterSlave.getMode() == MS_MODE_MASTER) {
     this->rgbLed.setLEDColor(&ColorBlue);
-
+    this->leftMotor.on();
+    this->rightMotor.on();
   } else {
     this->rgbLed.setLEDColor(&ColorRed);
-
-
-
+    this->leftMotor.off();
+    this->rightMotor.off();
   }
 
 
-  /*
-
-    if (lightLeft > ((lightRight * 18) / 16))
-    {
-      Serial.println("turn right");
-
-        this->leftMotor.on();
-        this->rightMotor.off();
-
-    }
-    else   if (lightRight > ((lightLeft * 18) / 16))
-    {
-      Serial.println("turn left");
-        this->leftMotor.off();
-        this->rightMotor.on();
-    }
-    else
-    {
-
-
-       this->leftMotor.off();
-        this->rightMotor.off();
-    }*/
-  //printData("Left: ",this->adc.getLightValue(0));
-  //printData("Right: ",this->adc.getLightValue(1));
-
 }
 
 
-void SwarmBot::executeState(void)
-{
-
-
-}
 
 
--- SwarmBot.h
+++ SwarmBot.h
@@ -3,30 +3,18 @@
 
 
 
-typedef enum botState_e {BOT_STATE_MOVING_LEFT, BOT_STATE_MOVING_RIGHT, BOT_STATE_RESTING};
-
-const uint8_t STATE_MASTER = 0x01;
-const uint8_t STATE_SLAVE = 0x02;
 
 class SwarmBot : public Vibrabot {
 
 protected:
-//  botState_e state;
 
-   uint16_t counter = 0;
-   uint16_t noMasterTokenCounter = 3000;
-
-   uint8_t state = STATE_MASTER;
 public:
   
 
   void init(void);
   void process(void);
   
-  protected:
-  void checkMaster(void);
-  void sendMsg(void);
-  void checkLight(void);
-  void executeState(void);
+ protected:
+  void updateLED(void);
 
 };
